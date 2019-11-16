35dd0914ec0dd2d2391d5fa398782fe934a0ac94
Ryan <cybervoid41@gmail.com>
2019-11-16 10:21:10 -0800

    initial commit

---
+++ SurveyProject/SurveyProject/Controllers/SurveyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SurveyProject.Models;
+
+namespace SurveyProject.Controllers
+{
+    public class SurveyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Survey model)
+        {
+            return View("Index", model);
+        }
+    }
+}
---
+++ SurveyProject/SurveyProject/Models/Survey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyProject.Models
+{
+    public class Survey
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+        public string FavoriteFood { get; set; }
+    }
+}

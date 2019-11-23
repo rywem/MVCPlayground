d1d7e34ca4fe7bafbc4e3f6ecae6b03f7de477d8
Ryan <cybervoid41@gmail.com>
2019-11-23 11:32:01 -0800

    Initial commit, repository pattern

---
+++ UpliftSolution/Uplift.DataAccess/Data/Repository/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uplift.DataAccess.Data.Repository.IRepository;
+namespace Uplift.DataAccess.Data.Repository
+{
+    class UnitOfWork : IUnitOfWork
+    {
+    }
+}

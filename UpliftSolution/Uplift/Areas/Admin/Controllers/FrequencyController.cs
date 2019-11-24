eaf3ee1eb82b36a9a409c61ac20bb60294606c7c
Ryan <cybervoid41@gmail.com>
2019-11-23 21:04:01 -0800

    Implemented Upsert and Delete functionality for Frequency

--- UpliftSolution/Uplift/Areas/Admin/Controllers/FrequencyController.cs
+++ UpliftSolution/Uplift/Areas/Admin/Controllers/FrequencyController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
 
 namespace Uplift.Areas.Admin.Controllers
 {
@@ -21,12 +22,59 @@ namespace Uplift.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Upsert(int? id)
+        {
+            Frequency frequency = new Frequency();
+
+            if(id == null)            
+                return View(frequency);
+
+            frequency = _unitOfWork.Frequency.Get(id.GetValueOrDefault());
+
+            if (frequency == null)
+                return NotFound();
+
+            return View(frequency);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Frequency frequency)
+        {
+            if( ModelState.IsValid)
+            {
+                if (frequency.Id == 0)
+                    _unitOfWork.Frequency.Add(frequency);
+                else
+                    _unitOfWork.Frequency.Update(frequency);
+
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(frequency);
+        }
+
+
         [HttpGet]
         public IActionResult GetAll()
         {
             return Json(new { data = _unitOfWork.Frequency.GetAll() });
         }
 
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Frequency.Get(id);
+
+            if (objFromDb == null)
+                return Json(new { success = false, message = "Error while deleting, Id does not exist. " });
+
+            _unitOfWork.Frequency.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete successful." });
+        }
 
     }
 }
--- UpliftSolution/Uplift/wwwroot/js/frequency.js
+++ UpliftSolution/Uplift/wwwroot/js/frequency.js
@@ -1,1 +1,66 @@
-﻿
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url" : "/admin/frequency/GetAll",
+            "type": "GET",
+            "datatype" : "json"
+        },
+        "columns": [
+            { "data": "name", "width": "50%" },
+            { "data": "frequencyCount", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/Admin/Frequency/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width 100px'>
+                                    <i class='fas fa-pen'></i> Edit
+                                </a>
+                                &nbsp;
+                                <a onclick=Delete("/Admin/Frequency/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100px'>
+                                    <i class='fas fa-trash'></i> Delete
+                                </a>
+                                &nbsp;
+                            </div>                            
+                            `;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable" : "No records found"
+        },
+        "width" : "100%"
+    });
+}
+
+function Delete(url) {
+    // Use Sweet Alert to give popup confirming delete.
+    swal({
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to restore the content!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, Delete it.",
+        closeOnconfirm: true
+    }, function () {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message);
+                    dataTable.ajax.reload();
+                }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+        });
+    });
+}

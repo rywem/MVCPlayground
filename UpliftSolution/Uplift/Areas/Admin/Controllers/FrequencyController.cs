c1edc83c35e3e5b305e18c045853c65d5acc0ad3
Ryan <cybervoid41@gmail.com>
2019-11-23 17:32:35 -0800

    FrequencyController and Index View added

--- UpliftSolution/Uplift/Areas/Admin/Controllers/FrequencyController.cs
+++ UpliftSolution/Uplift/Areas/Admin/Controllers/FrequencyController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
 
 namespace Uplift.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class FrequencyController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FrequencyController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Frequency.GetAll() });
+        }
+
+
     }
 }

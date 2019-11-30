88ea1769e0dcf7c7c3e9cb97d0f4e234d2895bec
Ryan <cybervoid41@gmail.com>
2019-11-30 10:08:33 -0800

    Added Register actions

--- UpliftSolution/Uplift/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ UpliftSolution/Uplift/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -13,7 +13,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
-
+using Uplift.Models;
+using Uplift.Utility;
 namespace Uplift.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
@@ -23,17 +24,15 @@ namespace Uplift.Areas.Identity.Pages.Account
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
-
-        public RegisterModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public RegisterModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager,
+                            ILogger<RegisterModel> logger, IEmailSender emailSender, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager;
         }
 
         [BindProperty]
@@ -60,6 +59,13 @@ namespace Uplift.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+            public string Name { get; set; }
+            public string StreetAddress { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string ZipCode { get; set; }
+
+            public string PhoneNumber { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -74,22 +80,46 @@ namespace Uplift.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser 
+                { 
+                    UserName = Input.Email, 
+                    Email = Input.Email,
+                    Name = Input.Name, 
+                    City = Input.City,
+                    StreetAddress = Input.StreetAddress, 
+                    State = Input.State,
+                    ZipCode = Input.ZipCode,
+                    PhoneNumber = Input.PhoneNumber
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+
+                    if (!await _roleManager.RoleExistsAsync(StaticDetails.Admin))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(StaticDetails.Admin));
+                        await _roleManager.CreateAsync(new IdentityRole(StaticDetails.Manager));
+                    }
+
+                    string role = Request.Form["rdUserRole"].ToString();
+
+                    if (role == StaticDetails.Admin)
+                        await _userManager.AddToRoleAsync(user, StaticDetails.Admin);
+                    else if (role == StaticDetails.Manager)
+                        await _userManager.AddToRoleAsync(user, StaticDetails.Manager);
+
                     _logger.LogInformation("User created a new account with password.");
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code },
-                        protocol: Request.Scheme);
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { area = "Identity", userId = user.Id, code = code },
+                    //    protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
@@ -97,7 +127,7 @@ namespace Uplift.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        //await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
                     }
                 }

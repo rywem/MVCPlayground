7f618cfb967feafe52c1ed7dfa0b8f4d2e8da381
Ryan <cybervoid41@gmail.com>
2020-06-27 09:22:24 -0700

    GraphQL project stated, Database project completed

---
+++ MVC_GraphQL_Tutorial/RealEstateManager.Database/Models/Property.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateManager.Database.Models
+{
+    /// <summary>
+    /// https://www.youtube.com/watch?v=XoM1zHZug68&list=PLBMCyCQ4nalaWRYBDKrIOHkFK1Y5AxbrK&index=2
+    /// </summary>
+    public class Property
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public decimal Value { get; set; }
+        public string Family { get; set; }
+        public ICollection<Payment> Payments { get; set; }
+    }
+}
